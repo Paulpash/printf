2ef0b0737897a3f626e604f4d5e93ea07c974104
Paul_Lubega <lubegapaul39@gmail.com>
2022-10-17 15:48:07 -0700

    added d and i formats

--- _printf.c
+++ _printf.c
@@ -14,6 +14,8 @@ int _printf(const char *format, ...)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
 	};
 	va_list arg_list;
 
