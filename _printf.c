6e5fc3c6e69184f7b16cabf60c1bdc85663fa0aa
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2022-10-17 15:00:50 -0700

    Added %c %s and % formatters for _printf

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formatted string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+	};
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
+}
