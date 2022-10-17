5b527584e3f69e277110e7499ea1942289fd0367
Paul_Lubega <lubegapaul39@gmail.com>
2022-10-17 13:09:08 -0700

    added prototype to main.h

--- main.h
+++ main.h
@@ -7,5 +7,5 @@
 int _printf(const char *format, ...);
 int _write_char(char c);
 int parser(const char *format, conver_t f_list[], va_list arg_list);
-
+int print_number(va_list params);
 #endif /* MAIN_H */
