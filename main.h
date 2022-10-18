86095579a699384a7db701d0015981a915e2867a
Paul_Lubega <lubegapaul39@gmail.com>
2022-10-18 12:28:50 -0700

    added int rot13(va_list list) and int print_reversed(va_list arg) to main.h

--- main.h
+++ main.h
@@ -37,6 +37,8 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
+int print_reversed(va_list arg);
+int rot13(va_list list);
 
 int hex_check(int, char);
 
